dce9c543bc71d76ef8c8cd6733ff1086dd7f578f
teplicate <teplicate@gmai.com>
2018-11-21 23:49:24 +0500

    finished

--- src/main/kotlin/Gauss.kt
+++ src/main/kotlin/Gauss.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 class GCD {
     fun findGcd(a: Int, b: Int): Int {
         var x = a
@@ -6,7 +8,6 @@ class GCD {
         while (x != 0 && y != 0) {
             if (x == y)
                 return x
-//            println("$x, $y")
             if (x > y)
                 x %= y
             else if (x < y)
@@ -24,20 +25,34 @@ class LCM {
     }
 }
 
+fun <T> Array<T>.swap(first: Int, second: Int) {
+    val t = this[first]
+    this[first] = this[second]
+    this[second] = t
+}
+
 class GaussLinearEquation {
     fun solve(data: Array<Array<Int>>): List<Double>? {
+        val copy = data.copyOf()
+        copy.forEachIndexed { index, array ->
+            if (array[0] == 0 && index != 0) {
+                if (index + 1 < array.size)
+                    data.swap(index, index + 1)
+            }
+        }
+
         val directPassageRes = performDirectPassage(data)
         directPassageRes.removeIf { it.all { s -> s == 0 } }
 
-        if (!checkIfSolvable(directPassageRes))
+        if (checkIfSolvable(directPassageRes)) {
             return null
+        }
 
-        if (checkIfInfiniteSolutions(directPassageRes))
+        if (checkIfInfiniteSolutions(directPassageRes)) {
             return emptyList()
-
+        }
         val vars = performInversePassage(directPassageRes)
 
-        println(vars)
         return vars
     }
 
@@ -49,11 +64,8 @@ class GaussLinearEquation {
 
     private fun checkIfSolvable(directPassageRes: ArrayList<Array<Int>>): Boolean {
         return directPassageRes.any { arr ->
-            arr.filterIndexed { index, i ->
-                if (index != arr.size - 1)
-                    i == 0
-                else true
-            }.isNotEmpty()
+            val s = arr.sliceArray(0..arr.size - 2)
+            s.all { it == 0 }
         }
     }
 
@@ -78,7 +90,7 @@ class GaussLinearEquation {
             map.put(currentMemberIdx--, x)
             variables.add(x)
         }
-        println(map)
+
         return variables
     }
 
@@ -97,6 +109,9 @@ class GaussLinearEquation {
                 val a1 = baseCopy[i]
                 val a2 = nextEquation[i]
                 var c = 1
+
+                if (a2 == 0 || a1 == 0)
+                    continue
 
                 val leastCommonMultiplier = lcm.findLCM(Math.abs(a1), Math.abs(a2))
 
@@ -126,15 +141,95 @@ class GaussLinearEquation {
 
         return directPassage
     }
+
+    fun getData(): Array<Array<Int>> {
+        Scanner(System.`in`).use { sc ->
+            val line = sc.nextInt()
+            val vars = sc.nextInt()
+
+            val list = ArrayList<Array<Int>>()
+
+            for (i in 0 until line) {
+                val array = Array(vars + 1)
+                {
+                    sc.nextInt()
+                }
+                list.add(array)
+            }
+
+            return list.toTypedArray()
+        }
+    }
 }
 
-fun main(args: Array<String>) {
-//    val res = GCD().findGcd(4, 2)
-//    println(res)
-    val data = arrayOf(
-        arrayOf(1, -1, -5),
-        arrayOf(2, 1, -7)
-    )
 
-    GaussLinearEquation().solve(data)
+fun main(args: Array<String>) {
+    val gauss = GaussLinearEquation()
+    val data = gauss.getData()
+    val result = gauss.solve(data)
+
+    when {
+        result == null -> println("NO")
+        result.isEmpty() -> println("INF")
+        else -> {
+            val trabs = result.map {
+                val wh = it.toLong() - it
+                if (result.all { wh == 0.toDouble() })
+                    it.toInt()
+                else it.toFloat()
+            }
+            println("YES")
+            val s = trabs.reversed().joinToString(" ")
+            println(s)
+        }
+    }
+}
+
+class Test {
+
+    fun test() {
+        val gauss = GaussLinearEquation()
+        val data = arrayOf(
+            arrayOf(4, 2, 1, 1),
+            arrayOf(7, 8, 9, 1),
+            arrayOf(9, 1, 3, 2)
+        )
+
+        val data2 = arrayOf(
+            arrayOf(1, 3, 4, 4),
+            arrayOf(2, 1, 4, 5)
+        )
+
+        val data3 = arrayOf(
+            arrayOf(1, 3, 2, 7),
+            arrayOf(2, 6, 4, 8),
+            arrayOf(1, 4, 3, 1)
+        )
+
+        val data4 = arrayOf(
+            arrayOf(6, 1, 2, 21),
+            arrayOf(4, -6, 16, 2),
+            arrayOf(3, 8, 1, 2)
+        )
+
+        val data5 = arrayOf(
+            arrayOf(5, -3, 2, -8, 1),
+            arrayOf(1, 1, 1, 1, 0),
+            arrayOf(3, 5, 1, 4, 0),
+            arrayOf(4, 2, 3, 1, 3)
+        )
+
+        val data6 = arrayOf(
+            arrayOf(1, 2, 3, 3),
+            arrayOf(3, 5, 7, 0),
+            arrayOf(1, 3, 4, 1)
+        )
+
+        gauss.solve(data)
+        gauss.solve(data2)
+        gauss.solve(data3)
+        gauss.solve(data4)
+        gauss.solve(data5)
+        gauss.solve(data6)
+    }
 }
